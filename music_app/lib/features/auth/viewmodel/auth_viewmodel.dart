56132d4def7600f4a86cae5413383a331c37b2fc
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2024-07-27 12:58:17 +0100

    fixing version conflict problem

--- music_app/lib/features/auth/viewmodel/auth_viewmodel.dart
+++ music_app/lib/features/auth/viewmodel/auth_viewmodel.dart
@@ -1,5 +1,11 @@
+import 'package:music_app/features/auth/model/user_model.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'auth_viewmodel.g.dart';
 
 @riverpod
-class AuthViewModel extends _$AuthViewModel {}
+class AuthViewModel extends _$AuthViewModel {
+  @override
+  AsyncValue<UserModel>? build() {
+    return null;
+  }
+}
---
+++ music_app/lib/features/auth/viewmodel/auth_viewmodel.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_viewmodel.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$authViewModelHash() => r'cfc28ff4dfcc0454f43dc1366a7d27642c9fe415';
+
+/// See also [AuthViewModel].
+@ProviderFor(AuthViewModel)
+final authViewModelProvider =
+    AutoDisposeNotifierProvider<AuthViewModel, AsyncValue<UserModel>?>.internal(
+  AuthViewModel.new,
+  name: r'authViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AuthViewModel = AutoDisposeNotifier<AsyncValue<UserModel>?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
