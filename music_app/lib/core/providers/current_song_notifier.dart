e3205fe08ad288cf4cad7dcf5f4b7dba4d390570
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2024-08-05 18:00:06 +0100

    audio seek ui

--- music_app/lib/core/providers/current_song_notifier.dart
+++ music_app/lib/core/providers/current_song_notifier.dart
@@ -7,6 +7,7 @@ part 'current_song_notifier.g.dart';
 @riverpod
 class CurrentSongNotifier extends _$CurrentSongNotifier {
   AudioPlayer? audioPlayer;
+  bool isplaying = false;
   @override
   SongModel? build() {
     return null;
@@ -19,8 +20,30 @@ class CurrentSongNotifier extends _$CurrentSongNotifier {
     final audioSource = AudioSource.uri(
       Uri.parse(song.song_url),
     );
-    audioPlayer!.setAudioSource(audioSource);
 
+    audioPlayer!.playerStateStream.listen((state) {
+      if (state.processingState == ProcessingState.completed) {
+        audioPlayer!.seek(Duration.zero);
+        audioPlayer!.pause();
+        isplaying = false;
+
+        this.state = this.state?.copyWith(hex_code: this.state?.hex_code);
+      }
+    });
+
+    audioPlayer!.setAudioSource(audioSource);
+    isplaying = true;
     state = song;
+  }
+
+  void playAndPause() {
+    if (isplaying) {
+      audioPlayer?.pause();
+    } else {
+      audioPlayer?.play();
+    }
+
+    isplaying = !isplaying;
+    state = state?.copyWith(hex_code: state?.hex_code);
   }
 }
--- music_app/lib/features/home/view/widget/audio_slab.dart
+++ music_app/lib/features/home/view/widget/audio_slab.dart
@@ -11,82 +11,129 @@ class AudioSlab extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final currentSong = ref.watch(currentSongNotifierProvider);
+    final songNotifier = ref.read(currentSongNotifierProvider.notifier);
     if (currentSong == null) {
       return const SizedBox();
     }
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height * 0.07,
-      decoration: BoxDecoration(
-        color: hexToColor(currentSong.hex_code),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
+    return Stack(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.07,
+          decoration: BoxDecoration(
+            color: hexToColor(currentSong.hex_code),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Hero(
-                  tag: 'music-image',
-                  child: Container(
-                    width: 48,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(
-                          currentSong.thumbnail_url,
-                        ),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 8),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
+                Row(
                   children: [
-                    Text(
-                      currentSong.song_name,
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
+                    Hero(
+                      tag: 'music-image',
+                      child: Container(
+                        width: 48,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              currentSong.thumbnail_url,
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                          borderRadius: BorderRadius.circular(4),
+                        ),
                       ),
                     ),
-                    Text(
-                      currentSong.artist,
-                      style: const TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        color: Pallete.subtitleText,
+                    const SizedBox(width: 8),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          currentSong.song_name,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          currentSong.artist,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            color: Pallete.subtitleText,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () async {},
+                      icon: const Icon(
+                        CupertinoIcons.heart,
+                        color: Pallete.whiteColor,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        songNotifier.playAndPause();
+                      },
+                      icon: Icon(
+                        songNotifier.isplaying
+                            ? CupertinoIcons.pause_fill
+                            : CupertinoIcons.play_fill,
+                        color: Pallete.whiteColor,
                       ),
                     ),
                   ],
                 )
               ],
             ),
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () async {},
-                  icon: const Icon(
-                    CupertinoIcons.heart,
-                    color: Pallete.whiteColor,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    CupertinoIcons.play_fill,
-                    color: Pallete.whiteColor,
-                  ),
-                ),
-              ],
-            )
-          ],
+          ),
         ),
-      ),
+        StreamBuilder(
+            stream: songNotifier.audioPlayer?.positionStream,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const SizedBox();
+              }
+              final position = snapshot.data;
+              final duration = songNotifier.audioPlayer!.duration;
+              double sliderValue = 0.0;
+              if (position != null && duration != null) {
+                sliderValue = position.inMilliseconds / duration.inMilliseconds;
+              }
+
+              return Positioned(
+                bottom: 0,
+                left: 8,
+                child: Container(
+                  height: 2,
+                  width: sliderValue * (MediaQuery.of(context).size.width - 32),
+                  decoration: BoxDecoration(
+                    color: Pallete.whiteColor,
+                    borderRadius: BorderRadius.circular(7),
+                  ),
+                ),
+              );
+            }),
+        Positioned(
+          bottom: 0,
+          left: 8,
+          child: Container(
+            height: 2,
+            width: MediaQuery.of(context).size.width - 32,
+            decoration: BoxDecoration(
+              color: Pallete.inactiveSeekColor,
+              borderRadius: BorderRadius.circular(7),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
